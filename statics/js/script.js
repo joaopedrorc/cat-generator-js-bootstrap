40516198311573548f6d0e51e6acc4c22ec8e5f2
Joao Pedro <jpc5626@gmail.com>
2020-02-18 20:12:32 -0300

    Projeto 2

---
+++ statics/js/script.js
@@ -0,0 +1,8 @@
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://thecatapi.com/api/images/get?format=src&type=gif&size=smal";  
+    div.appendChild(image);
+}
+
+console.log('teste')
